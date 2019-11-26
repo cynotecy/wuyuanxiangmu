30b524930a4cf70b69c05ebbcf958a9f079df06e
Asslass <asslass.4062@gmail.com>
2019-11-26 18:10:00 +0800

    频谱图绘图离线测试到位

---
+++ postgraduate_program/python3.5/function/plotWithCursor.py
@@ -0,0 +1,82 @@
+# coding=UTF-8
+import matplotlib
+import numpy as np
+from PyQt5.QtWidgets import *
+from matplotlib.backends.qt_compat import QtWidgets
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4 import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from matplotlib.widgets import Cursor
+import sys
+
+class getPos(QWidget):
+    def __init__(self, path, q, parent=None):
+        super(getPos, self).__init__(parent)
+        matplotlib.rcParams['font.family'] = ['SimHei']  # 用来正常显示中文标签
+        matplotlib.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+        self.path = path
+        self.q = q
+        layout = QtWidgets.QVBoxLayout(self)  # 实例化布局，用来塞画布
+        self.freqCanvas = FigureCanvas(Figure(figsize=(12, 6)))  # 实例化画布
+        self.freqCanvas.figure.canvas.mpl_connect('button_press_event', self.on_key_press)  # 事件绑定至响应函数
+
+        self.toolbar = NavigationToolbar(self.freqCanvas, self)  # 添加工具栏
+        layout.addWidget(self.toolbar)  # 布局塞入工具栏
+        layout.addWidget(self.freqCanvas)  # 布局塞入画布
+
+        self.axs = self.freqCanvas.figure.add_subplot(111)  # 从画布中实例化axs
+        self.cursor = Cursor(self.axs, useblit=True, color='red', linewidth=2)  # 实例化游标
+
+        self.draw()
+
+    def getData(self):
+        print(self.path)
+        file = open(self.path)
+
+        # x = file.readline().split(" ")
+        xy = file.read().split('\n')
+        x = xy[0]
+        x = x.split(' ')
+        y = xy[1]
+        y = y.split(' ')
+        x.pop(len(x) - 1)
+        y.pop(len(y) - 1)
+        # print(y)
+        # print(y[0:5])
+        # x = range(len(y))
+        x = np.array([float(x) for x in x])
+        y = np.array([float(y) for y in y])
+        return x, y
+
+    def draw(self):
+        x, y = self.getData()
+        self.axs.cla()
+        self.axs.plot(x, y)
+        self.axs.figure.canvas.draw()
+        self.axs.set_xlabel('频率/MHz', fontsize=14)
+        self.axs.set_ylabel('功率/dBM', fontsize=14)
+
+    # 定义响应函数
+    def on_key_press(self, event):
+        pos = event.ydata
+        if not self.q.empty():
+            self.q.get()
+            self.q.put(pos)
+        else:
+            self.q.put(pos)
+        print('print in on_key_press:' + str(pos))
+        # print(event.key)
+
+
+
+
+if __name__ == '__main__':
+    qapp = QtWidgets.QApplication(sys.argv)
+    x = range(300)
+    y = range(300)
+    import queue
+    q = queue.Queue()
+    path = r"..\..\usrp_recvfiles\specfiles\20191126155337.dat"
+    app = getPos(path, q)
+    app.show()
+    qapp.exec_()
--- postgraduate_program/python3.5/socketDemo/localDemoUi.py
+++ postgraduate_program/python3.5/socketDemo/localDemoUi.py
@@ -1,12 +1,14 @@
 import sys
 from threading import Thread
 import os
+import sip
 import zmq
 from PyQt5.QtCore import Qt, QFileInfo, QTimer
 from PyQt5.QtGui import QCursor, QIcon, QGuiApplication
 from PyQt5.QtWidgets import QMainWindow, QSystemTrayIcon, QMessageBox, QTableWidgetItem, QFileDialog, QProgressDialog, \
     QApplication
 
+from function import plotWithCursor
 from Ui.UitoPy.Ui_socketDEMO import Ui_MainWindow
 from function.numOrLetters import *
 from socketDemo import zmqLocal
@@ -18,6 +20,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         # py2 = py2Thread()
         # py2.run()
+        self.specPicFlag = 0
         self.zmqLocal = zmqLocal.localZMQ()
         self.pushButton_1.clicked.connect(self.on_pushButton_clicked_1)  # 扫频
         # self.pushButton_5.clicked.connect(self.on_pushButton_clicked_5)  # 选择文件
@@ -46,6 +49,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                         "本地连接超时！",
                                         QMessageBox.Yes,
                                         QMessageBox.Yes)
+                else:
+                    specPath = reslt
+                    # 根据地址绘图
+
+                    #####置入绘图####
+                    if self.specPicFlag:
+                        self.verticalLayout.removeWidget(self.getPosition)
+                        sip.delete(self.getPosition)
+                        self.getPosition = plotWithCursor.getPos(self.freq_list, self.bins, self.picQ)
+                        self.verticalLayout.addWidget(self.getPosition)
+                        self.specPicFlag = 1
+                    else:
+                        self.getPosition = plotWithCursor.getPos(self.freq_list, self.bins, self.picQ)
+                        self.verticalLayout.addWidget(self.getPosition)
+                        self.specPicFlag = 1
             else:
                 QMessageBox.warning(self,
                                     '错误',
