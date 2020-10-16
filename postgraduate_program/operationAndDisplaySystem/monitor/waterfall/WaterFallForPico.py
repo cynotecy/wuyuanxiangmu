6471635df96470d03980ead51764e7e346a7069f
asslass <asslass.4062@gmail.com>
2020-10-16 15:23:27 +0800

    新增pico瀑布图（单独运行）

--- postgraduate_program/operationAndDisplaySystem/monitor/waterfall/WaterFallForPico.py
+++ postgraduate_program/operationAndDisplaySystem/monitor/waterfall/WaterFallForPico.py
@@ -30,16 +30,20 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from matplotlib.collections import LineCollection
-
+from monitor.waterfall.annotation.dbOperation import *
+from function.dbInfo import dbInfo
+from function.filesOrDirsOperate import *
 
 class ApplicationWindow(QWidget):
-    def __init__(self, path):
+    def __init__(self, path, fatherPath):
         super().__init__()
-        self.n = 2
+        self.setWindowTitle("[现场电磁干扰检测特征库系统]0~30MHz频谱监测")
+        self.n = 1
         self.id = 1
         self.bar = 0
         self.fileNum = 0
         self.path = path# 传入pico保存的文件夹地址
+        self.fatherPath = fatherPath
         # #####################
         # self.lists = os.listdir(self.path)
         # self.txt_list = []
@@ -67,12 +71,12 @@ class ApplicationWindow(QWidget):
         self.axs[1].set_xlim(150, 200)
         self.axs[1].set_ylim(-100, 0)
 
-
+        self.nowTime = str(int(time.time()))
         norm = matplotlib.colors.Normalize(-120, -60)
         # self.freqCanvas.figure.colorbar(line, ax=self.axs[1], norm=norm, orientation='horizontal')
         self.freqCanvas.figure.canvas.mpl_connect('button_press_event', self.drawFreq)
-        self.start = QtWidgets.QPushButton('start')
-        self.stop = QtWidgets.QPushButton('stop')
+        self.start = QtWidgets.QPushButton(u'开始监测')
+        self.stop = QtWidgets.QPushButton(u'停止检测')
 
         layout.addWidget(self.start)
         layout.addWidget(self.stop)
@@ -80,11 +84,29 @@ class ApplicationWindow(QWidget):
         self.start.clicked.connect(self._start)
         self.stop.clicked.connect(self._stop)
 
+
+        databaseInfo=dbInfo
+        self.conn = pymysql.connect(host=databaseInfo[0],  # ID地址
+                               port=databaseInfo[1],  # 端口号
+                               user=databaseInfo[2],  # 用户名
+                               passwd=databaseInfo[3],  # 密码
+                               db=databaseInfo[4],  # 库名
+                               charset=databaseInfo[5])  # 链接字符集
+        self.cur = self.conn.cursor()  # 创建游标
+        self.dbCheck()
+        self.filePrefix=""
+        # self.getFilePrefix()
+
         # 定时任务
         self._timer = self.freqCanvas.new_timer(300, [(self._update_canvas, (), {})])
 
     def _start(self):
-        self._timer.start()
+        if self.filePrefix != "":
+            self._timer.start()
+        else:
+            self.getFilePrefix()
+            if self.filePrefix != "":
+                self._start()
 
     def _stop(self):
         self._timer.stop()
@@ -95,7 +117,7 @@ class ApplicationWindow(QWidget):
 
     def drawFreq(self, event):  # 点击热力图重绘拼频谱图
         po = event.ydata
-        self.axs[1].set_ylim(-500, 0)
+        # self.axs[1].set_ylim(-500, 0)
         if po == None:
             print("提示:点击位置不对")
         elif -po > self.n or po > 0:
@@ -103,7 +125,7 @@ class ApplicationWindow(QWidget):
         else:
             print(int(-po))
             x, y = self.getOldData(int(-po))
-            if not x == "noFile":
+            if x != "noFile":
                 self.axs[0].cla()
                 self.axs[0].plot(x, y)
                 self.axs[0].set_ylim(-140, -50)
@@ -114,47 +136,108 @@ class ApplicationWindow(QWidget):
                 self.axs[0].set_ylabel('功率/dBm',fontsize=14)
 
     def getOldData(self, n):
-        path = self.path + '\\' + '1 (' + str(n) +').csv'
+        if n == 1:
+            path = self.fileSavingPath + '\\' + self.filePrefix.strip(" ") + '.csv'
+        else:
+            path = self.fileSavingPath + '\\' + self.filePrefix+'(' + str(n) +').csv'
         if not path == "noFile":
             if os.path.exists(path):
                 data = pd.read_csv(path, skiprows=5)
-                x = data.values[15:2050, 0]
-                y = data.values[15:2050, 1]
+                x = data.values[15:-3, 0]
+                y = data.values[15:-3, 1]
                 return x, y
         else:
             x = "noFile"
             y = "noFile"
             return x, y
 
-    # def get_latest_file(self):
-    #     # lists = os.listdir(self.path)
-    #     # txt_list = []
-    #     # for path in lists:
-    #     #     if '.csv' in path:
-    #     #         txt_list.append(path)
-    #     # print(1111111)
-    #     # if txt_list and len(txt_list)>3:
-    #     #     txt_list.sort(key=lambda fn: os.path.getmtime(self.path + "\\" + fn))
-    #     #     print(len(txt_list))
-    #         # for txt in txt_list[0:-3]:
-    #         #     print(txt)
-    #         #     os.remove(self.path+'\\'+txt)
-    #     file_latest = os.path.join(self.path, self.txt_list[self.n])
-    #     self.n += 1
-    #     print(file_latest)
-    #     return file_latest
-    #     # else:
-    #     #     print('else')
-    #     #     return "noFile"
+    def getFilePrefix(self):
+        # walk self.path to get file prefix
+        fileList = os.listdir(self.path)
+        if fileList:
+            self.filePrefix = ''
+            for file in fileList:
+                if os.path.splitext(file)[1] == ".csv":
+                    fullFileName = os.path.splitext(file)[0]
+                    if "(" in fullFileName:
+                        self.filePrefix = fullFileName.split("(")[0]
+                    else:
+                        self.filePrefix = fullFileName + " "
+                    break
+                else:
+                    pass
+            if self.filePrefix == '':
+                QMessageBox.warning(self, "错误", "文件夹"+self.path+"中无csv型数据文件")
+                # self.close()
+        else:
+            QMessageBox.warning(self, "错误", "文件夹" + self.path + "中无csv型数据文件")
+            # self.close()
+
+    def initialDB(self):
+        # 新建表格和本地文件夹
+        dirName = self.nowTime
+        self.relativeDirPath = os.path.join('waterfall', 'pico', dirName)  # 数据文件相对地址（数据表中存的地址）
+        self.fileSavingPath = os.path.join(self.fatherPath, 'waterfall', 'pico', dirName)  # 数据文件绝对地址（存储动作的地址）
+        tableName = 'waterfall_data_pico_{}'.format(dirName)  # 数据表名
+        try:
+            # build the dir
+            makesureDirExist(self.fileSavingPath)
+            # build the table
+            create = (
+                "CREATE TABLE `{}`(`id` varchar(40) primary key, `create_time` bigint, `data_path` varchar(225))".format(
+                    tableName))
+            # print(create)
+            self.cur.execute(create)
+            self.conn.commit()
+        except:
+            QMessageBox.warning(self, '错误：', '初始化失败！')
+
+    def dbCheck(self):
+        """
+        查询输入的usrp号名下的瀑布图数据库表，将数据库表按时间戳排序，
+        保留时间最近的四张表，删除其余的表格和对应的文件夹,
+        新建带有当前时间戳的表格
+        Returns:
+            tableName:新的表名
+            dirPath:存储文件夹
+            relativeDirPath:存储文件夹地址的数据库格式
+
+        """
+        dirsPath = os.path.join(self.fatherPath, 'waterfall', 'pico')
+        makesureDirExist(dirsPath)
+        try:
+            dirList = list(os.walk(dirsPath))[0][1]
+            # print(dirList)
+        except Exception as e:
+            print(e)
+        else:
+            # 删除超出存储限度的表格和本地文件夹
+            if len(dirList) > 4:
+                for dir in dirList[0:-3]:
+                    try:
+                        dirPathToDrop = os.path.join(self.fatherPath, 'waterfall', 'pico', dir)
+                        if os.path.exists(dirPathToDrop):
+                            shutil.rmtree(dirPathToDrop)
+                        # drop db record
+                        drop = ("DROP TABLE `waterfall_data_pico_{}`".format(dir))
+                        self.cur.execute(drop)
+                        self.conn.commit()
+                    except Exception as e:
+                        self.conn.rollback()
+                        print(e)
+        self.initialDB()
 
     def getData(self):
-        path = self.path + '\\'+ '1 (' + str(self.n) +').csv'
+        if self.n == 1:
+            path = self.path + '\\' + self.filePrefix.strip(" ") +  '.csv'
+        else:
+            path = self.path + '\\'+ self.filePrefix+'(' + str(self.n) +').csv'
         # print(path)
         if not path == "noFile":
             if os.path.exists(path):
                 data = pd.read_csv(path, skiprows=5)
-                x = data.values[15:2050, 0]
-                y = data.values[15:2050, 1]
+                x = data.values[15:-3, 0]
+                y = data.values[15:-3, 1]
                 # print(self.n)
                 return x, y
         else:
@@ -162,20 +245,24 @@ class ApplicationWindow(QWidget):
             y = "noFile"
             return x, y
 
+
+    @dbUpload()
     def draw(self):
+        print("num: " + str(self.n))
         if self.n >= -self.axs[1].get_ylim()[0]:
             self.axs[1].set_ylim(int(self.axs[1].get_ylim()[0] - 100), int(self.axs[1].get_ylim()[0]))
         try:
             # 重绘频谱图
-            print(self.path)
+            # print(self.path)
             self.fileNum = sum([len(x) for _, _, x in os.walk(os.path.dirname(self.path))])
-            if self.fileNum > 3 and self.fileNum > self.n+1:
-                print('start draw')
+            # if self.fileNum > 3 and self.fileNum > self.n+1:
+            if self.fileNum > 1:
+                # print('start draw')
                 x, y = self.getData()
                 if self.bar:
                     self.bar.remove()
                 self.bar = 0
-                print('successful getdata')
+                print('successfully getdata')
                 self.axs[0].cla()
                 self.axs[0].plot(x, y)
                 self.axs[0].set_ylim(-140, -50)
@@ -185,8 +272,8 @@ class ApplicationWindow(QWidget):
 
 
                 heat = np.array([-self.n for x in x])
-                self.n += 1
-                self.id += 1
+                # self.n += 1
+                # self.id += 1
                 points = np.array([x, heat]).T.reshape(-1, 1, 2)
                 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
@@ -203,14 +290,15 @@ class ApplicationWindow(QWidget):
                 self.bar = self.freqCanvas.figure.colorbar(line, ax=self.axs[1], norm=norm, orientation='horizontal')
                 self.axs[1].figure.canvas.draw()
                 self.axs[1].figure.canvas.flush_events()
-                print('successful draw')
+                # print('successful draw')
                 return line
-        except:
-            pass
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
     qapp = QtWidgets.QApplication(sys.argv)
-    app = ApplicationWindow(r'D:\myPrograms\CASTProgram\postgraduate_program\data\usrp_recvfiles\usrp_scan\\')
+    app = ApplicationWindow(r'D:\myPrograms\CASTProgram\postgraduate_program\data\pico-waterfall-data\\',
+                            "D:\myPrograms\CASTProgram\postgraduate_program\data\EMCfile")
     app.show()
     qapp.exec_()
---
+++ postgraduate_program/operationAndDisplaySystem/monitor/waterfall/annotation/dbOperation.py
@@ -0,0 +1,63 @@
+"""
+@File:dbOperation.py
+@Author:lcx
+@Date:2020/10/1422:38
+@Desc:pico瀑布图数据库入库注解
+"""
+from functools import update_wrapper
+from monitor.waterfall.compresse.LZ4 import *
+import shutil
+import os
+
+def dbUpload(dbField=['id','create_time', 'data_path']):
+    """
+    :param:无参数，直接调用类变量
+    :return:
+    """
+    def upload(f):
+        # f接函数对象
+        def load(self, *args, **kwds):
+            # 调用被注解函数
+            f(self, *args, **kwds)
+
+            # 数据文件存储
+            if self.n == 1:
+                dataPath = os.path.join(self.path, self.filePrefix.strip(" ") + ".csv")
+            else:
+                dataPath = os.path.join(self.path, self.filePrefix+"({}).csv".format(self.n))
+            if os.path.exists(dataPath) and self.fileNum>1:
+                ctime = int(os.stat(dataPath).st_ctime*1000)
+                shutil.move(dataPath, self.fileSavingPath)
+                (path, filename) = os.path.split(dataPath)
+
+                # 数据地址记录入库
+                tableName = "waterfall_data_pico_{}".format(self.nowTime)
+                try:
+                    insert = "INSERT INTO `{}`(`{}`,`{}`,`{}`) VALUES ('{}',{},'{}')".format(
+                        tableName, dbField[0], dbField[1], dbField[2], self.n,
+                        ctime, ("/file/"+ self.relativeDirPath+"/"+ filename))
+
+                    self.cur.execute(insert)
+                    self.conn.commit()
+                    self.n += 1
+                    self.id += 1
+                except:
+                    self.conn.rollback()
+                    if os.path.exists(os.path.join(self.fileSavingPath, filename)):
+                        os.remove(os.path.join(self.fileSavingPath, filename))
+        # 更新包裹函数，使其更像被包裹函数（我理解为将被包裹函数的参数传给包裹函数）
+        update_wrapper(load, f)
+        return load
+
+    return upload
+
+def dbDownload(dbConnection, tableName, createTime):
+    """
+    :param:dbConnection, tableName, createTime
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
